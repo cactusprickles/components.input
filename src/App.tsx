7e91a5ba602cebac09171b81826d7424cb77f614
Cattamoo <ouob77@kakao,com>
2023-01-24 21:57:32 +0900

    기본 Input 페이지로 묶음!

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import Input from "./components/Input/Input";
-import {useState} from "react";
-import {FaLock, FaUser} from "react-icons/fa";
+import DefaultInput from "./pages/DefaultInput";
 
 export default function App() {
-	const [input, setInput] = useState('');
-	const [password, setPassword] = useState('');
 	return (
 		<div>
-			<Input label={<FaUser />} placeholder="아이디" value={input} onChange={(event) => setInput(event.target.value)} />
-			<Input label={<FaLock />} placeholder="비밀번호" type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+			<DefaultInput />
 		</div>
 	);
 }
